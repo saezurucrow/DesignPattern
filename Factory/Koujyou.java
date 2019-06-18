0f2d5bacbc3962c6140e479d88a4ac2f2e312ea3
saezurucrow <usobukuhito@gmail.com>
2019-06-18 14:02:22 +0900

    Add Factory

---
+++ Factory/Koujyou.java
@@ -0,0 +1,12 @@
+
+abstract class Koujyou {
+
+	public final Seihin create() {
+		Seihin seihin = factoryMethod(); touroku(seihin);
+		return seihin;
+	}
+
+	public abstract Seihin factoryMethod();
+	public abstract void touroku(Seihin s);
+
+}
